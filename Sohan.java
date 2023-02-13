96f4925a920b0dea6bdcc7ceb10f4ed8a47d9691
sohanshrestha12 <sohanshrestha40@gmail.com>
2023-02-13 20:56:17 +0545

    Assignment-I

---
+++ Sohan.java
@@ -0,0 +1,14 @@
+public class Sohan {
+    public static void main(String[] args){
+        System.out.println("Name of the organization: Kamalpokhari Restaurant & Bar");
+        System.out.println();
+        System.out.println("Address:");
+        System.out.println("----State: KM");
+        System.out.println("----Zip Code: 44600");
+        System.out.println("----Country: Nepal");
+        System.out.println();
+        System.out.println("Contact Information:");
+        System.out.println("----Mobile Number: 9865382409");
+        System.out.println("----Email: Kamalpokhari.rnb@gmail.com");
+    }
+}
